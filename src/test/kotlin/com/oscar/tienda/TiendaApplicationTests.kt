13f1c6bf3a4e1fe597d924194f3639a8ef9f95c6
Oscar Leon <omleon@sudamericano.edu.ec>
2021-10-25 10:42:59 -0500

    first commit

---
+++ src/test/kotlin/com/oscar/tienda/TiendaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.oscar.tienda
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
